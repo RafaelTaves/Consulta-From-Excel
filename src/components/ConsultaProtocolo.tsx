008ec67143a20a7b0777a69fb52f9d1a139c769a
RafaelTaves <rafaeljosetaves@gmail.com>
2025-07-09 11:27:15 -0300

    Fix: api externa para comunicação com google sheets

--- src/components/ConsultaProtocolo.tsx
+++ src/components/ConsultaProtocolo.tsx
@@ -26,7 +26,7 @@ export default function ConsultaProtocolo() {
     setResultado(null)
 
     try {
-      const res = await fetch("http://localhost:3001/consulta", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/consulta`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
